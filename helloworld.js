df00251d55faa42aebc62a69ddf6d74cce3a116b
DeVriesArt <devriesart@gmail.com>
2017-03-22 22:10:31 +0200

    Revert "New functions were written"
    This reverts commit ace9c83617281088cb54b23106f9507de2d64e04.

--- helloworld.js
+++ helloworld.js
@@ -1,13 +1,1 @@
-console.log("hello world");var http = require('http');
-
-http.createServer(function (request, response) {
-    console.log('request starting...');
-
-    response.writeHead(200, { 'Content-Type': 'text/html' });
-
-    var html = '<center><div style="width:200px; height:200px; background-color:blue;"><form><input name="input" type="text" ><br><button>Submit</button></div></center>';
-
-    response.end(html, 'utf-8');
-}).listen(8125);
-
-console.log('Server running at http://127.0.0.1:8125/');
+console.log("hello world");
